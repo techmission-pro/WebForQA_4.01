f6a7d9af7425dd9ee8e2afef20942c4ae994ca3e
Tabletka18 <nakonechniy4@gmail.com>
2023-01-08 15:39:16 +0200

    Update calc.js
    New functions in calc

--- resources/calc.js
+++ resources/calc.js
@@ -1,8 +1,22 @@
-document.querySelector('.calc-calculate').addEventListener('click', function(){
+document.querySelector('.calc-calculate').addEventListener('click', function() {
 	const num1 = +document.querySelector('[name="num1"]').value;  
 	const num2 = +document.querySelector('[name="num2"]').value;  
+	const op = document.querySelector('[name="op"]').value; 
 
-	const sum = num1 + num2 
+	let result;
 
-	document.querySelector('.calc-result').innerText = sum; 
+	if (op ===	'plus') {
+		result = num1 + num2;
+	} else if (op === 'minus') {
+		result = num1 - num2;
+	} else if (op === 'multyply') {
+		result = num1 * num2;
+	} else if (op === 'divide') {
+		result = num1 / num2;
+	} else {
+		console.error('Wrong op');
+		alert('Wrong operation is selected'); 
+	}
+
+	document.querySelector('.calc-result').value = result; 
 });
