d12b600523f59545d4ff865cba5c44f37bad779f
MarySkryp <mkurdylo@gmail.com>
2023-01-09 01:27:39 +0200

    changes in calculator

--- resources/calc.js
+++ resources/calc.js
@@ -1,7 +1,21 @@
 document.querySelector('.calc-calculator').addEventListener('click', function() {
 	const num1= +document.querySelector('[name="num1"]').value;
 	const num2= +document.querySelector('[name="num2"]').value;
+	const op= document.querySelector('[name="op"]').value;
 	
-	const sum = num1 + num2;
-	document.querySelector('.calc-result').value = sum;
+	let result;
+	if (op=== 'plus'){
+		result = num1+num2;
+	} else if (op=== 'minus'){
+		result = num1-num2;
+	} else if (op=== 'multiply'){
+		result = num1*num2;
+	} else if (op=== 'divide'){
+		result = num1/num2;
+	} else{
+		console.error('Wrong op!');
+		alert('Wrong operstion is selected');
+	}
+		
+	document.querySelector('.calc-result').value = result;
 });
