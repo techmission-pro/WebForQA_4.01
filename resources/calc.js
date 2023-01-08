471289311111cb79958369ce480502df73746284
Wasileuss <wasyl.lviv@gmail.com>
2023-01-08 12:21:46 +0200

    Added multifunction calculator

--- resources/calc.js
+++ resources/calc.js
@@ -1,8 +1,22 @@
 document.querySelector('.calc-calculate').addEventListener('click', function () {
 	const num1 = +document.querySelector('[name="num1"]').value;
 	const num2 = +document.querySelector('[name="num2"]').value;
+	const op = document.querySelector('[name="op"]').value;
 
-	const sum = num1 + num2;
+	let result;
 
-	document.querySelector('.calc-result').value = sum;
+	if (op === 'plus') {
+		result = num1 + num2;
+	} else if (op === 'minus') {
+		result = num1 - num2;
+	} else if (op === 'multiply') {
+		result = num1 * num2;
+	} else if (op === 'divide') {
+		result = num1 / num2;
+	} else {
+		console.error('Wrong op!');
+		alert('Wrong operation is selected')
+	}
+
+	document.querySelector('.calc-result').value = result;
 })
