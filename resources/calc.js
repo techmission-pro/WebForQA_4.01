e7e45c0d2696b972625929e105fe8cd80ff0ca68
mihaliuk <sergios170393@gmail.com>
2023-01-08 22:16:24 +0200

    Calculator of two numbers with different operations

--- resources/calc.js
+++ resources/calc.js
@@ -1,8 +1,22 @@
 document.querySelector('.calc-calculate').addEventListener('click', function() {
     const num1 = document.querySelector('[name= "num1"]').value;
     const num2 = document.querySelector('[name="num2"]').value;
-    
-    const sum = Number(num1) + Number(num2);
+    const op = document.querySelector('[name="op"]').value;
 
-    document.querySelector('.calc-result').value = sum;
+    let result;
+
+    if(op === "plus") {
+        result = Number(num1) + Number(num2);
+    } else if(op === "minus") {
+        result = num1 - num2;
+    } else if(op === "multiply") {
+        result = num1 * num2;
+    } else if(op === "devide") {
+        result = num1 / num2;
+    } else {
+        console.error("Wron op!");
+        alert("Wron operation is selected!");
+    }
+
+    document.querySelector('.calc-result').value = result;
 })
