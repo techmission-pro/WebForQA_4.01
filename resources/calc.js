469b38fab496acef4a012dfbc9913ab7cf661883
Horda <kosta.horda@gmail.com>
2023-01-10 16:23:31 +0200

    Changes to the functionality of the calculator: the ability to perform calculations not specified by the user has been removed (unfilled fields are not calculated)
    Editing of the style sheet in the selection
    of paired elements for the previous homework

--- resources/calc.js
+++ resources/calc.js
@@ -1,29 +1,29 @@
 function calculate (){
-    const num1 = +document.querySelector('[name="num1"]').value;
-    const num2 = +document.querySelector('[name="num2"]').value;
+    const num1 = document.querySelector('[name="num1"]').value;
+    const num2 = document.querySelector('[name="num2"]').value;
     const op = document.querySelector('[name="op"]').value;
 
     let result;
 
-    if (op ==='plus') {
-        result = num1 + num2;
+    if (op ==='plus') {                
+        result = parseInt (num1) + parseInt (num2);
     }
     else if (op === 'minus'){
-        result = num1 - num2;
+        result = parseInt (num1) - parseInt (num2);
     }
     else if (op === 'multiply') {
-        result = num1 * num2;
+        result = parseInt (num1) * parseInt (num2);
     }
     else if (op === 'divide') {
-        result = num1 / num2;
+        result = parseInt (num1) / parseInt (num2);
     }
     else {
         console.error('Wrong op!');
         alert('Wrong operation is selected');
 
-    }
+    } 
 
-    const sum = num1 + num2;
+    //const sum = num1 + num2;
 
     document.querySelector('.calc-result').value = result;
 }
