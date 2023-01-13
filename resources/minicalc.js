e0d5612fc6ac4c4c313f5a2f91ece8cdccf22b45
arseniivitalii <arseniivitalii@gmail.com>
2023-01-13 14:37:20 +0200

    added auto result, added button clear, change style of block whith calc.

--- resources/minicalc.js
+++ resources/minicalc.js
@@ -1,6 +1,7 @@
 function calculate() {
   const firstNumber = document.getElementById('firstNumber').value;
   const secondNumber = document.getElementById('secondNumber').value;
+  
   const oper = document.getElementById('opt').value;
 
  let result;
@@ -23,3 +24,25 @@ function calculate() {
   }
   document.querySelector('[name="result"]').value = result;
 };
+document.getElementById('opt').addEventListener('change',function (){ 
+  calculate()
+})
+document.getElementById('firstNumber').addEventListener('input',function (){ 
+  if (isNaN(document.getElementById('firstNumber').value)) {
+    alert('Please enter number only')
+    document.getElementById('firstNumber').value = '';
+  }
+  calculate()
+})
+document.getElementById('secondNumber').addEventListener('input',function (){ 
+  if (isNaN(document.getElementById('secondNumber').value)) {
+    alert('Please enter number only')
+    document.getElementById('secondNumber').value = '';
+  }
+  calculate()
+})
+document.getElementById('clear').addEventListener('click', () => {
+  document.getElementById('firstNumber').value = '';
+  document.getElementById('secondNumber').value = '';
+  document.querySelector('[name="result"]').value = '';
+});
