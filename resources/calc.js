d18ca4976927e45a56536ac85fc7bd07f35c2043
Pavlo Androsiuk <pasaandrosuk@gmail.com>
2023-01-17 20:22:27 +0100

    Add Calculate

---
+++ resources/calc.js
@@ -0,0 +1,39 @@
+function culculate() {
+	const num1 = +document.querySelector('[name="num1"]').value;
+	const num2 = +document.querySelector('[name="num2"]').value;
+	const op =  document.querySelector('[name="op"]').value;
+	
+	let result;
+	
+	if (op === 'plus') {
+	    result = num1 + num2;
+	} else if (op === 'minus') {
+	    result = num1 - num2;
+	} else if (op === 'multiply') {
+	    result = num1 * num2; 
+	} else if (op === 'divide') {
+	    result = num1 / num2; 
+	} else {
+	    console.error('Wrong op!');
+		alert('Wrong operation is selected');
+	}
+	
+	document.querySelector('.calc-result').value = result;
+}
+
+document.querySelector('.calc-calculate').addEventListener('click', function() {
+	culculate();
+});
+
+document.querySelector('[name="op"]').addEventListener('change', function() {
+	culculate();
+});
+
+document.querySelector('[name="num1"]').addEventListener('input', function() {
+	culculate();
+});
+
+document.querySelector('[name="num2"]').addEventListener('input', function() {
+	culculate();
+});
+
