4b774b61ca26fa6e9566daf892decf4dd70eb2e3
Yurii Polozhevets <yi.polozhevets@gmail.com>
2023-01-17 19:38:09 +0200

    Added events have been to input fields, operation selection, and button. Added added check when dividing by 0

--- resources/calc.js
+++ resources/calc.js
@@ -1,23 +1,37 @@
-document.querySelector('.calc-calculate').addEventListener('click', function(){
+function calculate(){
     const num1 = +document.querySelector('[name="num1"]').value;
     const num2 = +document.querySelector('[name="num2"]').value;
     const op = document.querySelector('[name="op"]').value;
-    
-let result;
+        
+    let result;
+        if(op === 'plus'){
+            result = num1 + num2;
+        }else if(op === 'minus'){
+            result = num1 - num2;
+        }else if(op === 'multiply'){
+            result = num1 * num2;
+        }else if(op === 'divide' && num2 != 0){
+            result = num1 / num2;
+        }else{
+            console.error('Wrong op!!!');
+            alert('Wrong oreration is selected');
+            }
 
-if(op === 'plus'){
-    result = num1 + num2;
-}else if(op === 'minus'){
-    result = num1 - num2;
-}else if(op === 'multiply'){
-    result = num1 * num2;
-}else if(op === 'divide'){
-    result = num1 / num2;
-}else{
-    console.error('Wrong op!!!');
-    alert('Wrong oreration is selected');
+        
+    document.querySelector('.calc-result').value = result;
 }
 
-
-document.querySelector('.calc-result').value = result;
-});
+    document.querySelector('.calc-calculate').addEventListener('click', function(){
+        calculate();
+    });
+    
+    document.querySelector('[name="op"]').addEventListener('change', function(){
+        calculate();
+    });
+    
+    document.querySelector('[name="num1"]').addEventListener('input', function(){
+        calculate();
+    });
+    document.querySelector('[name="num2"]').addEventListener('input', function(){
+        calculate();
+    });
