7736a8023d25a797035477a4eafe76feb524c4a9
TetianaRomanova <rozavetrov7171@gmail.com>
2023-01-18 19:45:31 +0200

    Calculator

---
+++ resources/calc.js
@@ -0,0 +1,22 @@
+console.log('Calc loaded');
+document.querySelector('.calc-calculate').addEventListener('click', function() {
+const num1 = +document.querySelector('[name="num1"]').value;
+const num2 = +document.querySelector('[name="num2"]').value;
+const op = document.querySelector('[name="op"]').value;
+
+let result;
+
+if (op === 'plus') {
+	result = num1 + num2;
+} else if (op === 'minus'){
+	result = num1 - num2;
+} else if (op === 'multiple') {
+	result = num1 * num2;
+} else if (op === 'divide') {
+	result = num1 / num2;	
+} else {
+	comsole.error('Wrong op!');
+	alert ('Wrong operation in selected');
+}
+document.querySelector('.calc-result').value = result;
+});
