42d9b0d6ab60e176c0d6ae110e8ff1524d5be3d8
TetianaRomanova <rozavetrov7171@gmail.com>
2023-01-18 20:53:04 +0200

    Automatic update of the result in calculate

--- resources/calc.js
+++ resources/calc.js
@@ -1,22 +1,39 @@
-console.log('Calc loaded');
-document.querySelector('.calc-calculate').addEventListener('click', function() {
-const num1 = +document.querySelector('[name="num1"]').value;
-const num2 = +document.querySelector('[name="num2"]').value;
-const op = document.querySelector('[name="op"]').value;
 
-let result;
 
-if (op === 'plus') {
-	result = num1 + num2;
-} else if (op === 'minus'){
-	result = num1 - num2;
-} else if (op === 'multiple') {
-	result = num1 * num2;
-} else if (op === 'divide') {
-	result = num1 / num2;	
-} else {
-	comsole.error('Wrong op!');
-	alert ('Wrong operation in selected');
+function calculate() {
+	const num1 = +document.querySelector('[name="num1"]').value;
+	const num2 = +document.querySelector('[name="num2"]').value;
+	const op = document.querySelector('[name="op"]').value;
+
+	let result;
+
+	if (op === 'plus') {
+		result = num1 + num2;
+	} else if (op === 'minus'){
+		result = num1 - num2;
+	} else if (op === 'multiple') {
+		result = num1 * num2;
+	} else if (op === 'divide') {
+		result = num1 / num2;	
+	} else {
+		comsole.error('Wrong op!');
+		alert ('Wrong operation in selected');
+	}
+	document.querySelector('.calc-result').value = result;
 }
-document.querySelector('.calc-result').value = result;
+
+document.querySelector('.calc-calculate').addEventListener('click', function () {
+	calculate ();
 });
+
+document.querySelector('[name="op"]').addEventListener('change', function () {
+	calculate ();
+});	
+
+document.querySelector('[name="num1"]').addEventListener('input', function () {
+	calculate ();
+});	
+
+document.querySelector('[name="num2"]').addEventListener('input', function () {
+	calculate ();
+});	
