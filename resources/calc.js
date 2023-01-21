d877632bc08c2470caaa73fdc5b6cd2035d58f55
Oleksandr Vozniuk <oleksandrvoznyk6@gmail.com>
2023-01-22 01:23:23 +0200

    operatoin select and result auto updating

--- resources/calc.js
+++ resources/calc.js
@@ -1,8 +1,22 @@
- document.getElementById("calc-calculate").addEventListener('click', function () {
+ function  calculate () {
     const num1 = +document.getElementById("num1").value;
     const num2 = +document.getElementById("num2").value;
+    const op = document.getElementById("op").value;
 
-    const sum = num1 + num2;
+    let result
+    if(op === 'plus') {
+      result = num1 + num2;
+     } else if (op === 'minus') {
+      result = num1 - num2;
+     } else if (op === 'multiply') {
+      result = num1 * num2;
+     } else if (op === 'divide') {
+      result = num1 / num2;
+     } else {
+      console.error('Wrong op!');
+      alert('Wrong operation is selected');
+     }
 
-    document.getElementById('calc-result').innerText = sum;
- })
+
+    document.getElementById('calc-result').innerText = result;
+ }
