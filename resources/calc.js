2fdc3305f925065ab24dd1a44e4a18ae3eb840f6
Vasilisa Zozulya <vasilisazozulya@macbook-pro.home>
2023-01-23 12:39:21 +0100

    task 4

---
+++ resources/calc.js
@@ -0,0 +1,38 @@
+function calculate() { 
+    const num1 = +document.querySelector('[name="num1"]').value;
+    const num2 = +document.querySelector('[name="num2"]').value;
+    const op = document.querySelector('[name="op"]').value;
+
+    let result;
+
+    if (op === 'plus') {
+        result = num1 + num2;
+    } else if (op === 'minus') {
+        result = num1 - num2;
+    } else if (op === 'multiply') {
+        result = num1 * num2;
+    } else if (op === 'divide') {
+        result = num1 / num2;
+    } else {
+        console.error('Wrong op!');
+        alert('Wrong operation is selected');
+    }
+
+    document.querySelector('.calc-result').value = result;
+}
+
+document.querySelector('.calc-calculate').addEventListener('click', function() {
+    calculate();
+});
+
+document.querySelector('[name="op"]').addEventListener('input', function() {
+    calculate();
+});
+
+document.querySelector('[name="num1"]').addEventListener('input', function() {
+    calculate();
+});
+
+document.querySelector('[name="num2"]').addEventListener('input', function() {
+    calculate();
+});
