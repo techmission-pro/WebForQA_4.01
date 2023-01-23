2cba2a91d676e111759b934af67bac549c1c31ab
Oryshkevych <angmar.black@gmail.com>
2023-01-23 22:09:21 +0200

    calc3

--- resources/calc.js
+++ resources/calc.js
@@ -1,4 +1,4 @@
-document.querySelector(`.calc-calculate`).addEventListener(`click`, function {} {
+document.querySelector(`.calc-calculate`).addEventListener(`click`, function () {
 	const num1 = +document.querySelector(`[name="num1"]`).value;
 	const num2 = +document.querySelector(`[name="num2"]`).value;
 	
