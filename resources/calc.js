4a67c37742c038215bb7eec5d80aeff6f8c8ff97
tatyanabaran <tetanabaran5@gmail.com>
2023-01-24 21:57:54 +0200

    add and change calculator

--- resources/calc.js
+++ resources/calc.js
@@ -1,9 +1,35 @@
-document.querySelector('.calc-calculate').addEventListener('click', function ()  {
+function calculate()  {
     const num1 = +document.querySelector ('[name="num1"]').value;
     const num2 = +document.querySelector ('[name="num2"]').value;
+    const op = document.querySelector ('[name="op"]').value;
 
-    const sum = num1 + num2;
+    let result;
 
-    document.querySelector('.calc-result').value = sum;
+    if (op === 'plus'){
+        result = num1 + num2;
+    } else if (op === 'minus') {
+        result = num1 - num2; 
+    } else if (op === 'multiply') {
+        result = num1 * num2;
+    } else if (op === 'divide') {
+        result = num1 / num2;
+    } else {
+        console.error('Wrong op!');
+        alert('Wrong operation is selected');
+    }
 
-});
+    document.querySelector('.calc-result').value = result;
+
+};
+document.querySelector('.calc-calculate').addEventListener ('click', function () {
+    calculate();
+})
+document.querySelector('[name="op"]').addEventListener ('change', function () {
+    calculate();
+})
+document.querySelector('[name="num1"]').addEventListener ('input', function () {
+    calculate();
+})
+document.querySelector('[name="num2"]').addEventListener ('input', function () {
+    calculate();
+})
