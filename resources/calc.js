01c0df89cceb1c7a5be663b18c78219cc100d229
Herasymenko <gera_new@ukr.net>
2023-01-26 00:10:12 +0200

    Create calc.js

---
+++ resources/calc.js
@@ -0,0 +1,44 @@
+//document.querySelector('.calc-calculate').addEventListener('click', function(){
+
+    function calculate() {
+        const num1=+document.querySelector('[name="num1"]').value;
+        const num2=+document.querySelector('[name="num2"]').value;
+        const op=document.querySelector('[name="op"]').value;
+    
+        let result;
+    if (op === 'plus') {
+        result = num1 + num2;
+    } else if (op === 'minus') {
+        result = num1 - num2;
+    }
+    else if (op === 'divide') {
+        result = num1 / num2;
+    }
+    else if (op === 'multiple') {
+        result = num1 * num2;
+    }
+    else{
+    console.error('wrong op');
+    alert('wrong');
+    }
+    
+        document.querySelector('.calc-result').value = result;
+     }
+     
+     
+    document.querySelector('.calc-calculate').addEventListener('click', function(){
+        calculate();
+    });
+    document.querySelector('[name="op"]').addEventListener('change', function(){
+        calculate();
+    });
+    document.querySelector('[name="num1"]').addEventListener('input', function(){
+        calculate();
+    });
+    document.querySelector('[name="num2"]').addEventListener('click', function(){
+        calculate();
+    });
+    
+    
+    
+    
