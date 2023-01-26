2836d05159699810d9e5cb23ef3d57bf6865759d
Kuzhel <romankuzhel@gmail.com>
2023-01-26 10:28:10 +0200

    calculator settings

--- resources/calc.js
+++ resources/calc.js
@@ -1,9 +1,35 @@
-document.querySelector('.calc-calculate').addEventListener('click', function () {
+function calculate() {
 	const num1 = +document.querySelector('[name="num1"]').value;
 	const num2 = +document.querySelector('[name="num2"]').value;
+		const op = document.querySelector('[name="op"]').value;
+		
+		let result;      
+		
+		if (op === 'plus') {
+	result = num1 + num2;
+} else if  (op === 'minus') {
+		result = num1 - num2;
+} else if (op === 'multiply') {
+		result = num1 * num2;
+}else if op === 'divide') {
+		result = num1 / num2;
+} else {
+	console.error('Wrong op!');
+	alert('Wrong operation is selected');
+}
 	
-	const sum = num1 + num2;
-	
-   document.querySelector('.calc-result').innerText = sum;
-});
+   document.querySelector('.calc-result').value = result;
+}
 
+document.querySelector('.calc-calculate').addEventListener('click',function(){
+	calculate();
+});
+document.querySelector('[name="op"]').addEventListener('change',function(){
+	calculate();
+});
+document.querySelector('[name="num1"]').addEventListener('input',function(){
+	calculate();
+});
+document.querySelector('[name="num2"]').addEventListener('input',function(){
+	calculate();
+});
